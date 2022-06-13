a30cf8282b39e8d7e7d509a5580f4dd38f83b550
ashwinisaulakhe <ashwinis@futurismtechnologies.com>
2022-06-13 19:31:43 +0530

    adding get post mapping on controller

--- src/main/java/com/filesystem/springapp/web/UserRegistrationController.java
+++ src/main/java/com/filesystem/springapp/web/UserRegistrationController.java
@@ -1,5 +1,31 @@
 package com.filesystem.springapp.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.filesystem.springapp.services.UserService;
+import com.filesystem.springapp.web.dto.UserRegistrationDto;
+
+@Controller
+@RequestMapping("/registration")
 public class UserRegistrationController {
 
+	private UserService userService;
+
+	public UserRegistrationController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	public String showRegistrationForm()
+	{
+		return "registration";
+	}
+	
+	public String registerUserAccount(@ModelAttribute("registration") UserRegistrationDto registrationDto)
+	{
+		userService.save(registrationDto);
+		return "redirect:/registration?success";
+	}
 }
