1246ff5f461e77bead3087a210f712d042c89c50
ashwinisaulakhe <ashwinis@futurismtechnologies.com>
2022-07-07 19:18:06 +0530

    changes in fileUploading controller & Adding helper class of file upload

--- src/main/java/com/filesystem/springapp/helper/FileUploadHelper.java
+++ src/main/java/com/filesystem/springapp/helper/FileUploadHelper.java
@@ -3,13 +3,8 @@ package com.filesystem.springapp.helper;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.google.common.io.Files;
 
 @Component
 public class FileUploadHelper {
@@ -19,19 +14,19 @@ public class FileUploadHelper {
 	{
 		boolean f=false;
 		try {
-			//InputStream is=multipartFile.getInputStream();
-			//byte data[]=new byte[is.available()];
-			//is.read(data);
+			InputStream is=multipartFile.getInputStream();
+			byte data[]=new byte[is.available()];
+			is.read(data);
 			
 			//write
-			//FileOutputStream fos =new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
-			//fos.write(data);
-			//fos.flush();
-			//fos.close();
+			FileOutputStream fos =new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
+			fos.write(data);
+			fos.flush();
+			fos.close();
 			
-			Files.copy(multipartFile.getInputStream(),
-					Paths.get( UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename()),
-					StandardCopyOption.REPLACE_EXISTING);
+			//Files.copy(multipartFile.getInputStream(),
+					//Paths.get( UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename()),
+					//StandardCopyOption.REPLACE_EXISTING);
 			f=true;
 		}
 		catch(Exception e)
